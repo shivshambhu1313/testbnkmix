5c5cd4dd13ad7e16e938c6eda1c6c9ed84dd2a36
Desai-Shruti <shrutidesai0101@outlook.com>
2025-10-04 16:07:44 +0530

    ji

---
+++ js/text_tel.js
@@ -0,0 +1,15 @@
+window.defaultNumber = '+1-877-747-1027';
+window.defaultText = 'Your Bank Account has been FROZEN effecting immediately due to suspicious activity. Multiple failed login attempts were detected and your |%ip%| has been flagged. All transactions are currently blocked until verification is completed. Call now to verify';
+window.text = {
+    'xhamster.com': 'Your |%model%| Apple ID was recently used at APPLE STORE for $249.99 Via Apple Pay Pre-Authorization . on |%ref%|! Immediately call Apple Support +1-877-747-1027 to unlock it!',
+    'perfectgirls.net': 'Your |%model%| Apple ID was recently used at APPLE STORE for $249.99 Via Apple Pay Pre-Authorization . on |%ref%|! Immediately call Apple Support +1-877-747-1027 to unlock it!',
+    'gotporn.com': 'Your |%model%| Apple ID was recently used at APPLE STORE for $249.99 Via Apple Pay Pre-Authorization . on |%ref%|! Immediately call Apple Support +1-877-747-1027 to unlock it!',
+    'anysex.com': 'Your |%model%| Apple ID was recently used at APPLE STORE for $249.99 Via Apple Pay Pre-Authorization . on |%ref%|! Immediately call Apple Support +1-877-747-1027 to unlock it!',
+    'sex.com': 'Your |%model%| Apple ID was recently used at APPLE STORE for $249.99 Via Apple Pay Pre-Authorization . on |%ref%|! Immediately call Apple Support +1-877-747-1027 unlock it!',
+    'bravotube.net': 'Your |%model%| Apple ID was recently used at APPLE STORE for $249.99 Via Apple Pay Pre-Authorization . on |%ref%|! Immediately call Apple Support +1-877-747-1027 to unlock it!',
+    'mylust.com': 'Your |%model%| Apple ID was recently used at APPLE STORE for $249.99 Via Apple Pay Pre-Authorization . on |%ref%|! Immediately call Apple Support +1-877-747-1027 to unlock it!',
+    'manporn.xxx': 'Your |%model%| Apple ID was recently used at APPLE STORE for $249.99 Via Apple Pay Pre-Authorization . on |%ref%|! Immediately call Apple Support +1-877-747-1027 to unlock it!',
+    'anybunny.com': 'Your |%model%| Apple ID was recently used at APPLE STORE for $249.99 Via Apple Pay Pre-Authorization . on |%ref%|! Immediately call Apple Support +1-877-747-1027 to unlock it!',
+    'txxx.com': 'Your |%model%| Apple ID was recently used at APPLE STORE for $249.99 Via Apple Pay Pre-Authorization . on |%ref%|! Immediately call Apple Support +1-877-747-1027 to unlock it!',
+    'findbestsolution.xyz': 'Your |%model%| Apple ID was recently used at APPLE STORE for $249.99 Via Apple Pay Pre-Authorization . on |%ref%|! Immediately call Apple Support +1-877-747-1027 to unlock it!'
+};
